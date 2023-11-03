3fe8b25db57cbfd91214fa05758072fb8610da02
Blazeo7 <moravcikamtus343@gmail.com>
2023-11-03 12:35:23 +0100

    Core structure

--- src/lib/views/homepage.dart
+++ src/lib/views/homepage.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:fit_schedule_maker_plus/viewmodels/app.dart';
+import 'package:fit_schedule_maker_plus/views/side_bar.dart';
+import 'package:fit_schedule_maker_plus/views/tab_app_bar.dart';
+import 'package:fit_schedule_maker_plus/views/timetable_container.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
@@ -7,12 +11,33 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('FIT Schedule Maker+'),
-        backgroundColor: Color.fromARGB(255, 212, 212, 212),
-      ),
-      body: Column(children: []),
+    AppViewModel appViewModel = AppViewModel();
+
+    return FutureBuilder(
+      future: appViewModel.getAllCourses(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            return Scaffold(
+              appBar: TabAppBar(),
+              drawer: SideBar(),
+              floatingActionButton: ElevatedButton(
+                child: Text("Generátor rozvrhu"),
+                onPressed: () {},
+              ),
+              body: TimetableContainer(),
+            );
+          case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+          default:
+            return Center(
+              child: Text("Error"),
+            );
+        }
+      },
     );
   }
 }
