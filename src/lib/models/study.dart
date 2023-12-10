aaba66534ac56fa76690b8d91e86d84f3c2346a3
Blazeo7 <moravcikamtus343@gmail.com>
2023-12-10 14:33:11 +0100

    Add duration & type attributes

--- src/lib/models/study.dart
+++ src/lib/models/study.dart
@@ -1,10 +1,30 @@
 import 'package:fit_schedule_maker_plus/models/program_course_group.dart';
 
+enum StudyType {
+  bachelor,
+  magister,
+}
+
+extension ParseToString on StudyType {
+  String toCzechString() {
+    switch (this) {
+      case StudyType.bachelor:
+        return "Bakalárske";
+      case StudyType.magister:
+        return "Magisterské";
+      default:
+        return "Unknown";
+    }
+  }
+}
+
 /// Represents a single study program at FIT.
 class StudyProgram {
   final int id;
   final String shortcut;
   final String fullName;
+  final StudyType type;
+  final int duration;
   List<ProgramCourseGroup> courseGroups;
 
   StudyProgram({
@@ -12,19 +32,24 @@ class StudyProgram {
     required this.fullName,
     required this.shortcut,
     required this.courseGroups,
+    required this.type,
+    required this.duration,
   });
 
-  factory StudyProgram.fromJson(Map<String, dynamic> json) => StudyProgram (
-        id: json["id"],
-        shortcut: json["shortcut"],
-        fullName: json["fullname"],
-        courseGroups: List<ProgramCourseGroup>.from(json["courseGroups"].map((group) => ProgramCourseGroup.fromJson(group)))
-      );
+  factory StudyProgram.fromJson(Map<String, dynamic> json) => StudyProgram(
+      id: json["id"],
+      shortcut: json["shortcut"],
+      fullName: json["fullname"],
+      type: json["type"],
+      duration: json["duration"],
+      courseGroups: List<ProgramCourseGroup>.from(json["courseGroups"].map((group) => ProgramCourseGroup.fromJson(group))));
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "shortcut": shortcut,
         "fullname": fullName,
+        "type": type,
+        "duration": duration,
         "courseGroups": List<dynamic>.from(courseGroups.map((group) => group.toJson()))
       };
 }
