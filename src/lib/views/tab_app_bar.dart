c8f6ee66a248dea696a3ba758f888bff902faa03
Blazeo7 <moravcikamtus343@gmail.com>
2023-12-11 14:49:56 +0100

    Add TabController

--- src/lib/views/tab_app_bar.dart
+++ src/lib/views/tab_app_bar.dart
@@ -5,19 +5,21 @@ const unsTabCol = Color.fromARGB(255, 125, 125, 125);
 const appBarCol = Color.fromARGB(255, 52, 52, 52);
 
 class TabAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const TabAppBar({super.key});
+  final TabController tabController;
+  const TabAppBar(this.tabController, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
         centerTitle: true,
         backgroundColor: appBarCol,
-        title: const TabBar(
+        title: TabBar(
+          controller: tabController,
           dividerColor: Colors.transparent,
           labelColor: Colors.white,
           unselectedLabelColor: unsTabCol,
           labelStyle: TextStyle(fontSize: 22),
-          tabs: <Widget>[
+          tabs: const <Widget>[
             Tab(text: 'Pracovní rozvrh'),
             Tab(text: 'Výsledný rozvrh'),
             Tab(text: 'Verze rozvrhu'),
