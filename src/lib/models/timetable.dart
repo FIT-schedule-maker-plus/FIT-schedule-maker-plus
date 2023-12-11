0bb6073dfdd67874ef1552eb993a334195ca8081
Jakub Kloub <xkloub03@stud.fit.vut.cz>
2023-12-11 18:17:51 +0100

    Add more interesing timetable methods.

--- src/lib/models/timetable.dart
+++ src/lib/models/timetable.dart
@@ -31,19 +31,46 @@ class Timetable {
     }
   }
 
-  bool containsLesson({required CourseID course, required LessonID lesson}) {
+  /// Check if current semester timetable contains a given course lesson.
+  bool containsLesson(CourseID course, LessonID lesson) {
     return currentContent.containsKey(course) &&
         currentContent[course]!.contains(lesson);
   }
 
+  /// Add course lesson to current semester timetable.
+  void addCourseLesson(CourseID course, LessonID lesson) {
+    if (currentContent[course] == null) {
+      currentContent[course] = {};
+    }
+    currentContent[course]!.add(lesson);
+  }
+
+  /// Remove lesson from current semester timetable.
+  void removeCourseLesson(CourseID course, LessonID lesson) {
+    if (!containsLesson(course, lesson)) {
+      return;
+    }
+    currentContent[course]!.remove(lesson);
+  }
+
+  /// Clear all lessons in current semester timetable.
+  void clearLessons() {
+    selected[semester] = {};
+  }
+
+  /// Add course to timetable, but without any selected lessons. This is used
+  /// for the case when user selected that they have a given course, but haven't selected
+  /// any lessons yet.
   void addCourse(CourseID courseID) {
     currentContent[courseID] = {};
   }
 
+  /// Remove course and all its course lessons from currect semester timetable.
   void removeCourse(CourseID courseID) {
     currentContent.remove(courseID);
   }
 
+  /// Check if given seme
   bool containsCourse(CourseID courseID) {
     return currentContent.containsKey(courseID);
   }
