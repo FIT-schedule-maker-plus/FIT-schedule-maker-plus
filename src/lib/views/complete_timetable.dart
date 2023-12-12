8805890695c61bde5f49fe3c73857fd408acf28c
Jakub Kloub <xkloub03@stud.fit.vut.cz>
2023-12-12 23:18:34 +0100

    Change watch to select in Courses

--- src/lib/views/complete_timetable.dart
+++ src/lib/views/complete_timetable.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fit_schedule_maker_plus/views/timetable_container.dart'
+    as custom_widget;
+
+import '../disp_timetable_gen.dart';
 
 class CompleteTimetable extends StatelessWidget {
   const CompleteTimetable({super.key});
@@ -6,7 +10,8 @@ class CompleteTimetable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color.fromARGB(255, 255, 0, 0),
+      color: const Color.fromARGB(255, 52, 52, 52),
+      child: const custom_widget.Timetable(filter: Filter.all()),
     );
   }
 }
