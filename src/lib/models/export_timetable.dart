310060891bbedf2a697448f875c55bc0b4b52d8d
Jakub Kloub <xkloub03@stud.fit.vut.cz>
2023-12-15 19:54:36 +0100

    Add comments to export_timetable.dart

--- src/lib/models/export_timetable.dart
+++ src/lib/models/export_timetable.dart
@@ -1,10 +1,22 @@
+/*
+ * Filename: export_timetable.dart
+ * Project: FIT-schedule-maker-plus
+ * Author: Jakub Kloub (xkloub03)
+ * Date: 15/12/2023
+ * Description: This file contains datastructure representing exported timetable as JSON.
+ */
+
 import 'package:fit_schedule_maker_plus/models/timetable.dart';
 
 import '../viewmodels/app.dart';
 
 /// Class for representing timetable to be exported.
 class ExportTimetable {
+  /// Timetable to export.
   Timetable timetable;
+
+  /// All program ids used byt this timetbale. This is used when loading the exported timetable,
+  /// we need to load all programs and their lessons that this timetable is using.
   List<int> programIds;
 
   ExportTimetable(this.timetable, this.programIds);
@@ -13,10 +25,13 @@ class ExportTimetable {
     required Timetable timetable,
     required AppViewModel avm,
   }) {
+    // All course ids used by this timetable.
     final usedCourseIds = timetable.selected.keys
         .map((k) => timetable.selected[k]!.keys)
         .expand((l) => l)
         .toList();
+
+    // Get all program IDs of used course IDs (get all programs the timetable uses).
     Set<int> programIds = avm.allStudyPrograms.keys.where((id) {
       return avm.allStudyPrograms[id]!.courseGroups.any(
         (group) => group.courses.any(
